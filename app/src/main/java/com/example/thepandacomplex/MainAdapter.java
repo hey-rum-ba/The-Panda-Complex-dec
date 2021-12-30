f7af856e766cddf120fafa622116cb594ba9877c
hey-rum-ba <siddhartcool2000@gmail.com>
2021-12-30 20:24:55 +0530

    initial commit

---
+++ app/src/main/java/com/example/thepandacomplex/MainAdapter.java
@@ -0,0 +1,45 @@
+package com.example.thepandacomplex;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.viewholder> {
+
+    ArrayList<MainModel> list;
+    Context context;
+
+    public MainAdapter(ArrayList<MainModel> list, Context context){
+        this.list =list;
+        this.context =context;
+    }
+
+    @NonNull
+    @Override
+    public viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.rentee_list,parent,false);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewholder holder, int position) {
+        final MainModel model = list.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class viewholder extends RecyclerView.ViewHolder {
+        public viewholder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
